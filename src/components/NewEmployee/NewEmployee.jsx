5e9f308b3f9ad0af51770fd3a2f320ab1422c452
Daniel Agustin Brosio <dbrosio@belatrix.com>
2019-08-02 13:04:22 -0300

    working happy path and started validation

--- src/components/NewEmployee/NewEmployee.jsx
+++ src/components/NewEmployee/NewEmployee.jsx
@@ -10,158 +10,133 @@ export class NewEmployee extends React.Component {
         }
     }
 
-
-/* 
-        fetch('http://localhost:8080/personAPI/employees', {
-            method: 'POST',
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json',
-              //'Access-Control-Allow-Origin': '*',
-
-            },
-            body: JSON.stringify({
-                firstNames: this.state.firtsName,
-                lastNames: this.state.lastName,
-                dateOfBirth: this.state.dateOfBirth,
-                documentType: this.state.docType,
-                documentID: this.state.docNum,
-                gender: this.state.gender,
-                nationality: this.state.nationality,        
-                contact: this.state.email,  
-                relationships: [],
-            })
-          })
-    }
- */
-
- 
-
     render() {
         return (
             <div className="bx-emp-form-container">
-                <div className="bx-emp-form-row">
-                    <div className="bx-emp-form-elem">
-                        <label>
-                            Name: 
+                <form >
+                    <div className="bx-emp-form-row">
+                        <div className="bx-emp-form-elem">
+                            <label>
+                                Name: 
+                                <input 
+                                    type="text" 
+                                    required 
+                                    name="first-name" 
+                                    placeholder="First name" 
+                                    onChange={this.onChangeInput('firstName')}
+                                />
+                            </label>
+                        </div>
+                        <div className="bx-emp-form-elem">
+                            <label >
+                                <input 
+                                    type="text" 
+                                    required 
+                                    name="last-name" 
+                                    placeholder="Last name"
+                                    onChange={this.onChangeInput('lastName')}
+                                />
+                            </label>
+                        </div>
+                    </div>
+                    <div className="bx-emp-form-row">
+                        <div className="bx-emp-form-elem">
+                            <label>
+                                Date of birth: 
+                                <input 
+                                    type="date" 
+                                    required 
+                                    name="date-of-birth"
+                                    onChange={this.onChangeInput('dateOfBirth')}
+                                />
+                            </label>
+                        </div>
+                    </div>
+                    <div className="bx-emp-form-row">
+                        <div className="bx-emp-form-elem">
+                            <label>
+                                Document: 
+                                <select 
+                                    name="doc-type" 
+                                    onChange={this.onChangeInput('docType')}
+                                >
+                                    <option value="-">-</option>
+                                    <option value="DNI">DNI</option>
+                                    <option value="LC">LC</option>
+                                </select>
+                            </label>
+                        </div>    
+                        <div className="bx-emp-form-elem">
+                            <label>
                             <input 
-                                type="text" 
+                                type="number" 
                                 required 
-                                name="first-name" 
-                                placeholder="First name" 
-                                onChange={this.onChangeInput('firstName')}
-                            />
-                        </label>
-                    </div>
-                    <div className="bx-emp-form-elem">
-                        <label >
-                            <input 
-                                type="text" 
-                                required 
-                                name="last-name" 
-                                placeholder="Last name"
-                                onChange={this.onChangeInput('lastName')}
-                            />
-                        </label>
-                    </div>
-                </div>
-                <div className="bx-emp-form-row">
-                    <div className="bx-emp-form-elem">
-                        <label>
-                            Date of birth: 
-                            <input 
-                                type="date" 
-                                required 
-                                name="date-of-birth"
-                                onChange={this.onChangeInput('dateOfBirth')}
-                            />
-                        </label>
-                    </div>
-                </div>
-                <div className="bx-emp-form-row">
-                    <div className="bx-emp-form-elem">
-                        <label>
-                            Document: 
-                            <select 
-                                name="doc-type" 
-                                onChange={this.onChangeInput('docType')}
-                            >
-                                <option value="-">-</option>
-                                <option value="DNI">DNI</option>
-                                <option value="LC">LC</option>
-                            </select>
-                        </label>
-                    </div>    
-                    <div className="bx-emp-form-elem">
-                        <label>
-                        <input 
-                            type="number" 
-                            required 
-                            name="doc-num" 
-                            onChange={this.onChangeInput('docNumber')}
-                        />                            
-                        </label>
-                    </div> 
-                </div>
-                <div className="bx-emp-form-row">
-                    <div className="bx-emp-form-elem">
-                        <label>
-                            Gender: 
-                            <select 
-                                name="gender"
-                                onChange={this.onChangeInput('gender')}
-                            >
-                                <option value="-">-</option>
-                                <option value="M">Male</option>
-                                <option value="F">Female</option>
-                                <option value="O">Other</option>
-                            </select>
-                        </label>
-                    </div>
-                    <div className="bx-emp-form-elem">
-                        <label>
-                            Nationality: 
-                            <select 
-                                name="nationality"
-                                onChange={this.onChangeInput('nationality')}
-                            >
-                                <option value="-">-</option>
-                                <option value="Argentina">Argentina</option>
-                                <option value="Brazil">Brazil</option>
-                                <option value="Chile">Chile</option>
-                                <option value="Uruguay">Uruguay</option>
-                            </select>
-                        </label>
-                    </div>
-                </div>
-                <div className="bx-emp-form-row">
-                    <div className="bx-emp-form-elem">
-                        <label>
-                            Phone: 
-                            <input 
-                                type="tel" 
-                                name="telphone" 
-                                placeholder="(+88) 888 8888 8888" 
-                                pattern="(+[0-9]{2})? [0-9]{3} [0-9]{4}-[0-9]{4}" 
-                                maxLength="18"
-                                onChange={this.onChangeInput('phone')}
+                                name="doc-num" 
+                                onChange={this.onChangeInput('docNumber')}
                             />                            
-                        </label>
+                            </label>
+                        </div> 
                     </div>
-                    <div className="bx-emp-form-elem">
-                        <label>
-                            E-mail: 
-                            <input 
-                                type="email" 
-                                name="email" 
-                                onChange={this.onChangeInput('email')}
-
-                            />
-                        </label>
+                    <div className="bx-emp-form-row">
+                        <div className="bx-emp-form-elem">
+                            <label>
+                                Gender: 
+                                <select 
+                                    name="gender"
+                                    onChange={this.onChangeInput('gender')}
+                                >
+                                    <option value="-">-</option>
+                                    <option value="M">Male</option>
+                                    <option value="F">Female</option>
+                                    <option value="O">Other</option>
+                                </select>
+                            </label>
+                        </div>
+                        <div className="bx-emp-form-elem">
+                            <label>
+                                Nationality: 
+                                <select 
+                                    name="nationality"
+                                    onChange={this.onChangeInput('nationality')}
+                                >
+                                    <option value="-">-</option>
+                                    <option value="Argentina">Argentina</option>
+                                    <option value="Brazil">Brazil</option>
+                                    <option value="Chile">Chile</option>
+                                    <option value="Uruguay">Uruguay</option>
+                                </select>
+                            </label>
+                        </div>
                     </div>
+                    <div className="bx-emp-form-row">
+                        <div className="bx-emp-form-elem">
+                            <label>
+                                Phone: 
+                                <input 
+                                    type="tel" 
+                                    name="telphone" 
+                                    placeholder="(+88) 888 8888 8888" 
+                                    pattern="(+[0-9]{2})? [0-9]{3} [0-9]{4}-[0-9]{4}" 
+                                    maxLength="18"
+                                    onChange={this.onChangeInput('phone')}
+                                />                            
+                            </label>
+                        </div>
+                        <div className="bx-emp-form-elem">
+                            <label>
+                                E-mail: 
+                                <input 
+                                    type="email" 
+                                    name="email" 
+                                    onChange={this.onChangeInput('email')}
+
+                                />
+                            </label>
+                        </div>
 
 
-                </div>
+                    </div>
+                </form>
             </div>
         
         );
--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -11,7 +11,7 @@ export class Popup extends React.Component {
             lastName: "",
             dateOfBirth: "",
             docType: "-",
-            docNum: "",
+            docNumber: "",
             gender: "-",
             nationality: "-",
             phone: "",
@@ -21,21 +21,122 @@ export class Popup extends React.Component {
 
     onChangeInputForm = (key, value) => {
         this.setState({[key]: value});
-        console.log (this.state)
+        // this.validateFields();
     }
 
-    validateFields(employeeData) {
-        Object.values(employeeData).forEach( (x) => {
-            console.log(x)
-        })
-    }
-        
-    onSaveEmployer = () => {
+    validateFields(empData) {
+        console.log(empData);
+        var today = new Date();
+        var minDate = new Date(1800,1,1);
 
-        console.log("onSaveEmployer")
-        if(this.validateFields(this.state)) {
-            this.props.saveEmployer(this.state);
+        const NameRegEx = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
+        const EmailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const PhoneRegEx = /^(([+]{1}[0-9]{2}|0)[0-9]{9})$/
+
+        if (NameRegEx.test(empData.firstName)) {
+            console.log("Valid first name");
+        } else {
+            console.log("Invalid first name");
+            return false;
         }
+
+        if (NameRegEx.test(empData.lastName)) {
+            console.log("Valid last name");
+        } else {
+            console.log("Invalid last name");
+            return false;
+        }
+
+ /*        if ( empData.dateOfBirth < today) {
+            console.log("Valid date of birth");
+        } else {
+            console.log("Invalid date of birth");
+            return false;
+
+        } */
+
+        if ( empData.docType !== "-") {
+            console.log("Valid docType");
+        } else {
+            console.log("Invalid docType");
+            return false;
+
+        }
+        console.log(empData.docNumber);
+
+        if ( Number(empData.docNumber > 0) ) {
+            console.log("Valid docNum");
+        } else {
+            console.log("Invalid docNum");
+            return false;
+        }
+
+        if ( empData.gender !== "-") {
+            console.log("Valid gender");
+        } else {
+            console.log("Invalid gender");
+            return false;
+
+        }
+        
+        if ( empData.nationality !== "-") {
+            console.log("Valid nationality");
+        } else {
+            console.log("Invalid nationality");
+            return false;
+
+        }
+        
+/* 
+        if (PhoneRegEx.test(empData.phone)) {
+            console.log("Valid phone");
+        } else {
+            console.log("Invalid phone");
+            return false;
+
+        } */
+
+        if (EmailRegEx.test(empData.email)) {
+            console.log("Valid e-mail");
+        } else {
+            console.log("Invalid e-mail");
+            return false;
+
+        }
+        return true;
+
+    }
+
+    onSaveEmployee = () => {
+
+        if(this.validateFields(this.state)) {
+            this.saveEmployee(this.state);
+        }
+    }
+
+    saveEmployee(empData) {
+
+        var objToSend = {};
+
+        objToSend["firstNames"] = empData.firstName;
+        objToSend["lastNames"] = empData.lastName;
+        objToSend["dateOfBirth"] = empData.dateOfBirth;
+        objToSend["documentType"] = empData.docType;
+        objToSend["documentID"] = empData.docNumber;
+        objToSend["gender"] = empData.gender;
+        objToSend["nationality"] = empData.nationality;
+        objToSend["contact"] = empData.email;
+        objToSend["relationships"] = [];      
+    
+        fetch('http://localhost:8080/personAPI/employees', {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'Access-Control-Allow-Origin': '*',
+            },
+            body: JSON.stringify(objToSend)
+          })
     }
     
     render() {
@@ -47,7 +148,7 @@ export class Popup extends React.Component {
                     </div>   
                     <div className='popup-content'>
                         <button onClick={this.props.closePopup}>Cancel</button>
-                        <button onClick={this.onSaveEmployer}>Save</button>
+                        <button onClick={this.onSaveEmployee}>Save</button>
                     </div>
                 </div>  
             </div>  
