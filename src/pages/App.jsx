7af2e64326af296e306a01b4ed81b114f6d3de46
Martin Mazagatti <mazagatti.martin@gmail.com>
2019-08-15 12:57:51 -0300

    Add Route for list all kinships

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,17 +3,16 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {store} from "../redux/createStore";
 import {PeopleList} from "./PeopleList";
-
-import '../App.css';
 import {KinshipList} from "./KinshipList";
+import '../App.css';
 
 class App extends React.Component {
     render () {
         return (
             <Provider store={store}>
                 <Router>
-                    <Route path="/" component={KinshipList} />
-                    <Route path="/kinships" component={KinshipList}/>
+                    <Route exact path="/" component={PeopleList} />
+                    <Route path="/kinships/" component={KinshipList} />
                 </Router>
             </Provider>
         );
