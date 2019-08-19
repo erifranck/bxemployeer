bfe218ca4c9886b992d336990131634ff5458c82
Martin Mazagatti <mazagatti.martin@gmail.com>
2019-08-19 12:17:39 -0300

    Fix Search field allowing unvalid characters and filter when field was empty

--- src/components/Searcher/Searcher.jsx
+++ src/components/Searcher/Searcher.jsx
@@ -7,48 +7,57 @@ import {searchKinship} from "../../redux/actions/kinshipActions";
 
 class SearchComponent extends React.Component {
     constructor(props) {
-       super(props);
-       this.state = {
-           filtered: '',
-       };
-       this.handleChange = this.handleChange.bind(this);
+        super(props);
+        this.state = {
+            filtered: '',
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.ignoreArrows = this.ignoreArrows.bind(this);
     }
+
     componentDidMount() {
         this.setState({
-          filtered: this.props.items
+            filtered: this.props.items
         });
     };
-      
+
     componentWillReceiveProps(nextProps) {
         this.setState({
-          filtered: nextProps.items
+            filtered: nextProps.items
         });
-    };
-
-    cleanInput() {
-        this.setState({
-            filtered: [],
-        });
-    };
-
-    onSearch = (event) => {
-        this.setState({filtered: event.target.value})
     };
 
     handleChange(event){
-        let val = event.target.value;
-        if(isNaN(val)){
+        let val = event.target.value.replace(/([^a-zA-z0-9]+)/g, s0 => '');
+        if(!(val === '')){
+            if(isNaN(val)){
+                if(!val.includes('\\') && !val.includes('[') && !val.includes(']'))
+                    this.props.searchKinship(val);
+            }
+            else {
+                this.props.searchPeople(val);
+            }
+        }
+        else{
+            this.props.searchPeople(val);
             this.props.searchKinship(val);
         }
-        else {
-            this.props.searchPeople(val);
+    };
+
+    ignoreArrows(event){
+        if (event.keyCode === 38 || event.keyCode === 40 || event.keyCode === 190 || event.keyCode === 69 || event.keyCode === 109 || event.keyCode === 110 || event.keyCode === 187 || event.keyCode === 107) {
+            event.preventDefault();
         }
     };
 
     render() {
         return (
             <div className="bx-searcher-container">
-                <input type="text" className="Searcher" onChange={this.handleChange}  placeholder="Search by document..."/>
+                { window.location.pathname === '/kinships' ?
+                    <input type="text" className="Searcher" onChange={this.handleChange}  placeholder="Search by employee source ..."/>
+                    :
+                    <input type="number" className="Searcher" onKeyDown={this.ignoreArrows} onChange={this.handleChange}  placeholder="Search by document ..."/>
+                }
             </div>
         )
     }
@@ -62,5 +71,5 @@ const mapDispatchToProps = (dispatch) => ({
 export const Searcher = connect(null,mapDispatchToProps)(SearchComponent);
 
 SearchComponent.propTypes = {
-   items: PropTypes.array.isRequired,
+    items: PropTypes.array.isRequired,
 };
