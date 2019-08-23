f17c122c4e90e139932dbff098be3177798cc230
Ivan Marcos Salomon <isalomon@belatrix.com>
2019-08-23 10:12:26 -0300

    homepage changes

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,19 +1,21 @@
 import React from "react";
 import {Button} from "../components/Button/Button"
 import { DropDownMenu } from "../components/Menu/dropDownMenu";
-import './personApi.css';
+import './homepage.css';
 
 export function Homepage(){
     return(
         <React.Fragment>
             <div className="bx-dashboard-wrapper">
                 <div className="bx-dashboard-container">
-                    <div className="menu">
-                        <li> <a href="/employee-list">Employees</a></li>
-                        <li> <a href="/kinships">Kinships</a></li>
-                        <li> <a href="/about">About</a></li>
+                    <div className="wrapper">
+                        <div className="menu">
+                            <li> <a href="/employee-list">Employees</a></li>
+                            <li> <a href="/kinships">Kinships</a></li>
+                            <li> <a href="/about">About</a></li>
+                        </div>
+                        <img src={require("../images/PersonApi.png")} className="logo"></img>
                     </div>
-                    <img src={require("../images/PersonApi.png")} className="personApi"></img>
                 </div>
             </div>
         </React.Fragment>
